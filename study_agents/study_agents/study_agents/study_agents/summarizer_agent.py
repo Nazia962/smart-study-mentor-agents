ff38e5a5f333091d38127d942ce0835a2f6a5534
Nazia Sultana <naziasultana0430@gmail.com>
2025-12-01 20:35:13 +0530

    Add summarizer_agent.py

---
+++ study_agents/study_agents/study_agents/study_agents/summarizer_agent.py
@@ -0,0 +1,4 @@
+# study_agents/summarizer_agent.py
+class SummarizerAgent:
+    def generate_summary(self, topic):
+        return f"{topic} is an important concept. This summary explains its basics in simple terms."
